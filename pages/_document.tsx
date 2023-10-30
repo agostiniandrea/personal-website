7fb3ab2fd2a2c44bb2bfd0bebc8b45a036e81080
Andrea Agostini <a.agostini92@gmail.com>
2023-10-30 15:38:33 +0100

    feat: add meticulous script

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,23 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      {(process.env.NODE_ENV === "development" ||
+        process.env.VERCEL_ENV === "preview") && (
+        // eslint-disable-next-line @next/next/no-sync-scripts
+        <Script
+          data-project-id="nQRPdOs7NUqDn1tx9wlwGak83ZWoKztKCLvqOuMj"
+          data-is-production-environment="false"
+          src="https://snippet.meticulous.ai/v1/meticulous.js"
+        />
+      )}
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
