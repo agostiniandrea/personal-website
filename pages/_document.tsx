a5a89fdf97f83783c568be60b67a5cf790ade1c2
Andrea Agostini <a.agostini92@gmail.com>
2023-11-12 12:49:44 +0100

    feat: remove meticulous

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,9 @@
-import { Html, Head, Main, NextScript } from "next/document";
-import Script from "next/script";
+import { Head, Html, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      {(process.env.NODE_ENV === "development" ||
-        process.env.VERCEL_ENV === "preview") && (
-        // eslint-disable-next-line @next/next/no-sync-scripts
-        <Script
-          data-project-id={process.env.METICULOUS_ACCESS_TOKEN}
-          data-is-production-environment="false"
-          src="https://snippet.meticulous.ai/v1/meticulous.js"
-        />
-      )}
       <body>
         <Main />
         <NextScript />
