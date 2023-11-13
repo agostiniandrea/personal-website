dc62da63d8c88490c2fde16db647124ad144bdcc
Andrea Agostini <andrea@thejibe.com>
2023-11-13 11:57:36 +0100

    feat: fonts update

--- config/customizations/globalStyles.ts
+++ config/customizations/globalStyles.ts
@@ -3,36 +3,19 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   @font-face {
-    font-family: 'Open Sans';
+    font-family: 'Rainier';
     font-weight: normal;
     font-style: normal;
-    src: url('/assets/fonts/static/Open_Sans/Open_Sans-Regular.woff') format('woff'), /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
-      url('/assets/fonts/static/Open_Sans/Open_Sans-Regular.woff2') format('woff2'); /* Chrome 26+, Opera 23+, Firefox 39+ */
-    font-display: swap;
-  }
-
-  @font-face {
-    font-family: 'Montserrat';
-    font-weight: normal;
-    font-style: normal;
-    src: url('/assets/fonts/static/Montserrat/Montserrat-Regular.woff'), /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
-      url('/assets/fonts/static/Montserrat/Montserrat-Regular.woff2') format('woff2'); /* Chrome 26+, Opera 23+, Firefox 39+ */
-    font-display: swap;
-  }
-
-  @font-face {
-    font-family: 'Lato';
-    font-weight: normal;
-    font-style: normal;
-    src: url('/assets/fonts/Lato/Lato-Regular.woff') format('woff'), /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
-      url('/assets/fonts/Lato/Lato-Regular.woff2') format('woff2'); /* Chrome 26+, Opera 23+, Firefox 39+ */
+    src: url('/assets/fonts/Rainier/RainierNorth500.woff') format('woff'), /* Chrome 6+, Firefox 3.6+, IE 9+, Safari 5.1+ */
+      url('/assets/fonts/Rainier/RainierNorth500.woff2') format('woff2'); /* Chrome 26+, Opera 23+, Firefox 39+ */
     font-display: swap;
   }
 
   body {
     background: ${theme.colors.background};
     color: ${theme.colors.main};
-    font-family: 'Open Sans', sans-serif;
+    font-size: 24px;
+    font-family: 'Rainier', sans-serif;
     margin: 0;
     overflow-x: hidden;
     padding: 0;
