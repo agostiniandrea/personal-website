1a48e599a20693697c49cabc4496931a407791f5
Andrea Agostini <a.agostini92@gmail.com>
2023-11-13 12:47:59 +0100

    feat: update components, 404 and utils

--- pages/404.tsx
+++ pages/404.tsx
@@ -5,6 +5,9 @@ import type { GetStaticPropsResult } from "next";
 import { Seo } from "@components/atoms";
 import { Container } from "@components/ions";
 import { TPageFields, getPageContent } from "@lib/utils/cms";
+import { useMedia } from "@lib/utils/useMedia";
+import { ThemeContext } from "styled-components";
+import { useContext } from "react";
 
 type TErrorPage = {
   page: TPageFields;
@@ -17,14 +20,36 @@ export async function getStaticProps(): Promise<GetStaticPropsResult<any>> {
 }
 
 export default function Error({ page }: TErrorPage) {
+  const { isMobile } = useMedia();
+  const themeContext = useContext(ThemeContext);
+
   return (
     <>
       <Seo seoDescription={page.seoDescription} seoTitle={page.seoTitle} />
-      <Header />
-      <Container verticalPadding>
-        <section>
-          <h1>404 - Page not found</h1>
-        </section>
+      <Header
+        styles={{
+          boxShadow: `0px -3px 0px 0px ${themeContext?.colors.headline} inset`,
+        }}
+      />
+      <Container
+        verticalPadding
+        styles={{
+          height: isMobile ? "482px" : "",
+        }}
+      >
+        <h1
+          style={{
+            color: themeContext?.colors.headline,
+            textAlign: "center",
+            fontSize: isMobile ? "40px" : "60px",
+            marginTop: isMobile ? "auto" : "",
+            marginBottom: isMobile ? "auto" : "",
+            paddingTop: isMobile ? "" : "200px",
+            paddingBottom: isMobile ? "" : "200px",
+          }}
+        >
+          404 - Page not found
+        </h1>
       </Container>
       <Footer />
     </>
