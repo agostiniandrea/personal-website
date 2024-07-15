fbee251800140829007e5cc596f33d41e39d39d9
Andrea Agostini <andrea@thejibe.com>
2024-07-15 11:47:28 +0200

    feat: update homepage and instagram post logic

--- components/molecules/InstagramPost/index.tsx
+++ components/molecules/InstagramPost/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import { theme } from "@config/theme";
 import { useMedia } from "@lib/utils/useMedia";
 import Image from "next/image";
 import Link from "next/link";
@@ -12,16 +11,6 @@ const StyledLink = styled(Link)`
   left: 0;
   right: 0;
   bottom: 0;
-`;
-
-const StyledVideo = styled.video`
-  ::-webkit-media-controls-volume-slider {
-    display: none;
-  }
-
-  ::-webkit-media-controls-mute-button {
-    display: none;
-  }
 `;
 
 type InstagramPostProps = {
@@ -36,16 +25,14 @@ const InstagramPost: React.FC<InstagramPostProps> = ({
   style,
   ...restProps
 }) => {
-  const [isHovered] = React.useState(false);
-
-  const { isMobile, isTablet, isDesktop } = useMedia();
+  const { isMobile, isTablet } = useMedia();
 
   const height = isMobile ? 350 : isTablet ? 300 : 500;
 
   return (
-    <div
+    <StyledLink
       {...restProps}
-      /* onMouseEnter={()=> setIsHovered(true)} onMouseLeave={()=> setIsHovered(false)} */ style={{
+      style={{
         cursor: "pointer",
         display: "flex",
         height: `${height}px`,
@@ -53,77 +40,21 @@ const InstagramPost: React.FC<InstagramPostProps> = ({
         width: "100%",
         ...style,
       }}
+      target="_blank"
+      href={media.permalink}
+      passHref
     >
-      <StyledLink href={media.permalink} target="_blank" key={media.id} />
-      {media.media_type === "VIDEO" && (
-        <StyledVideo
-          poster={media.thumbnail_url}
-          onMouseOver={(event) => (event.target as any).play()}
-          onMouseOut={(event) => {
-            (event.target as any).pause();
-            (event.target as any).currentTime = 0;
-          }}
-          preload="false"
-          loop
-          style={{
-            height: "100%",
-            width: "100%",
-            marginLeft: "auto",
-            marginRight: "auto",
-            objectFit: "cover",
-          }}
-          disablePictureInPicture
-          controls
-          controlsList="nodownload noplaybackrate nofullscreen"
-        >
-          <source src={media.media_url} type="video/mp4" />
-        </StyledVideo>
-      )}
-      {media.media_type === "IMAGE" && (
-        <Image
-          fill
-          style={{
-            marginLeft: "auto",
-            marginRight: "auto",
-            objectFit: "cover",
-          }}
-          src={media.media_url}
-          alt={media.caption}
-        />
-      )}
-      {media.media_type === "CAROUSEL_ALBUM" && (
-        <Image
-          fill
-          style={{
-            marginLeft: "auto",
-            marginRight: "auto",
-            objectFit: "cover",
-          }}
-          src={media.media_url}
-          alt={media.caption}
-        />
-      )}
-      <div
+      <Image
+        fill
         style={{
-          background:
-            "linear-gradient(90deg, rgba(255,255,255,0.75) 0%, rgba(255,255,255,0.03) 100%)",
-          opacity: isHovered ? "0.75" : "0",
-          transition: "opacity 0.25s ease-in-out",
-          position: "absolute",
-          padding: "0.5rem",
-          top: 0,
+          marginLeft: "auto",
+          marginRight: "auto",
+          objectFit: "cover",
         }}
-      >
-        <p
-          style={{
-            color: theme.colors.background,
-            fontSize: isDesktop ? "unset" : "16px",
-          }}
-        >
-          {media.caption}
-        </p>
-      </div>
-    </div>
+        src={media.thumbnail_url || media.media_url}
+        alt={media.caption}
+      />
+    </StyledLink>
   );
 };
 
