f5ffa00e7b31e141412bd61232f56448e7b1043b
Andrea Agostini <104834436+agostiniandrea@users.noreply.github.com>
2025-04-04 18:06:09 +0200

    fix: update theme context usage

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,13 +1,11 @@
-import { useContext } from "react";
-
 import { Seo } from "@components/atoms";
 import { Container } from "@components/ions";
 import { Footer, Header } from "@components/organisms";
+import { theme } from "@config/theme";
 import { PAGE_TYPES } from "@constants";
 import { TPageFields, getPageContent } from "@lib/utils/cms";
 import { useMedia } from "@lib/utils/useMedia";
 import type { GetStaticPropsResult } from "next";
-import { ThemeContext } from "styled-components";
 
 type TErrorPage = {
   page: TPageFields;
@@ -21,14 +19,13 @@ export async function getStaticProps(): Promise<GetStaticPropsResult<any>> {
 
 export default function Error({ page }: TErrorPage) {
   const { isMobile } = useMedia();
-  const themeContext = useContext(ThemeContext);
 
   return (
     <>
       <Seo seoDescription={page.seoDescription} seoTitle={page.seoTitle} />
       <Header
         styles={{
-          boxShadow: `0px -3px 0px 0px ${themeContext?.colors.headline} inset`,
+          boxShadow: `0px -3px 0px 0px ${theme?.colors.headline} inset`,
         }}
       />
       <Container
@@ -39,7 +36,7 @@ export default function Error({ page }: TErrorPage) {
       >
         <h1
           style={{
-            color: themeContext?.colors.headline,
+            color: theme?.colors.headline,
             textAlign: "center",
             fontSize: isMobile ? "40px" : "60px",
             marginTop: isMobile ? "auto" : "",
