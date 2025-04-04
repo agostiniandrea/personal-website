4d336f3b121939bb47e26e5360dc6462ed5ce4c2
Andrea Agostini <104834436+agostiniandrea@users.noreply.github.com>
2025-04-04 18:01:03 +0200

    chore: move analytics and speed insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,13 +2,19 @@ import { Head } from "@components/atoms";
 import GlobalStyle from "@config/customizations/globalStyles";
 import Theme from "@config/theme";
 import type { AppProps } from "next/app";
+import { Analytics } from '@vercel/analytics/next';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Theme>
-      <GlobalStyle />
-      <Head />
-      <Component {...pageProps} />
-    </Theme>
+    <>
+      <Theme>
+        <GlobalStyle />
+        <Head />
+        <Component {...pageProps} />
+      </Theme>
+      <SpeedInsights />
+      <Analytics />
+    </>
   );
 }
