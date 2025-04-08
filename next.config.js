c17f61924edcd21322b13ac06b0febcba07a4341
Andrea Agostini <104834436+agostiniandrea@users.noreply.github.com>
2025-04-08 12:17:37 +0200

    chore: update setup

--- next.config.js
+++ next.config.js
@@ -26,10 +26,8 @@ module.exports = withSentryConfig(nextConfig, {
   org: process.env.SENTRY_ORG,
   project: process.env.SENTRY_PROJECT,
 
-  // Only print logs for uploading source maps in CI
-  // Set to `true` to suppress logs
-  silent: !process.env.CI,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
-  disableLogger: true,
+  // Pass the auth token
+  authToken: process.env.SENTRY_AUTH_TOKEN,
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
 });
