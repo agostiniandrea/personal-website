27f43307236356990e51271ccabf2c91b177bffb
Andrea Agostini <104834436+agostiniandrea@users.noreply.github.com>
2025-05-01 17:15:30 +0200

    chore: update sentry setup

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
-/** @type {import('next').NextConfig} */
+import { config } from 'dotenv';
 import { withSentryConfig } from "@sentry/nextjs";
 
+// Load environment variables from .env.local file
+config(); // This loads variables into process.env
+
+/** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   images: {
@@ -22,12 +26,25 @@ const nextConfig = {
 };
 
 const sentryWebpackPluginOptions = {
+  // For all available options, see:
+  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+
   org: process.env.SENTRY_ORG,
   project: process.env.SENTRY_PROJECT,
+
+  // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
+
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
+
+  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers
   tunnelRoute: "/monitoring",
+
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
+
+  // Enables automatic instrumentation of Vercel Cron Monitors
   automaticVercelMonitors: true,
   authToken: process.env.SENTRY_AUTH_TOKEN,
   reactComponentAnnotation: {
