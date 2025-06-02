fb17cb70899b3f9f66f85365213b78e1974f42ea
Andrea Agostini <104834436+agostiniandrea@users.noreply.github.com>
2025-06-02 16:47:51 +0200

    chore: upgrade storybook to v.9

--- components/cms/HeroPrimary/HeroPrimary.stories.tsx
+++ components/cms/HeroPrimary/HeroPrimary.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from "@storybook/react";
+import type { Meta, StoryObj } from "@storybook/nextjs";
 import HeroPrimary from "./index";
 
 const meta: Meta<typeof HeroPrimary> = {
