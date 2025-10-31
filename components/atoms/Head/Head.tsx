ff8245629be1aa1b9aea9eb83672373fb7e03cb8
Andrea Agostini <104834436+agostiniandrea@users.noreply.github.com>
2025-10-31 12:09:59 +0700

    chore: update head rules and add middleware

--- components/atoms/Head/Head.tsx
+++ components/atoms/Head/Head.tsx
@@ -11,6 +11,7 @@ const Head = () => {
     <>
       <DefaultSeo {...configSEO} />
       <NextHead>
+        <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/site.webmanifest" key="site-manifest" />
         <link rel="icon" href="/favicon.ico" />
