0d2faeda918d892e71aa4b5690f33360152124c8
Andrea Agostini <104834436+agostiniandrea@users.noreply.github.com>
2026-05-25 21:04:20 +0700

    Revert "fix: add author meta tag to Head"
    This reverts commit 8067f5dcaffabfbd146d078aff5304345ecfece7.

--- components/atoms/Head/Head.tsx
+++ components/atoms/Head/Head.tsx
@@ -13,7 +13,6 @@ const Head = () => {
       <NextHead>
         <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1" />
         <meta property="og:type" content="website" />
-        <meta name="author" content="Andrea Agostini" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="manifest" href="/site.webmanifest" key="site-manifest" />
         <link rel="icon" href="/favicon.ico" />
